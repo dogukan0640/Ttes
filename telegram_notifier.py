83f43de023447183aa9be4367e628245ffd3ee01
dogukan0640 <otay0353@gmail.com>
2025-05-19 23:18:01 +0300

    Update telegram_notifier.py

---
+++ telegram_notifier.py
@@ -0,0 +1,21 @@
+import requests
+import os
+
+# Bu bilgileri senin Telegram botundan aldık
+BOT_TOKEN = "7744478523:AAEtRJar6uF7m0cxKfQh7r7TltXYxWwtmm0"
+CHAT_ID = "1009868232"
+
+def send_telegram_message(message):
+    try:
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        payload = {
+            "chat_id": CHAT_ID,
+            "text": message
+        }
+        response = requests.post(url, data=payload)
+        if response.status_code == 200:
+            print("✅ Telegram mesajı gönderildi.")
+        else:
+            print(f"⚠️ Telegram mesajı gönderilemedi: {response.text}")
+    except Exception as e:
+        print(f"❌ Telegram gönderim hatası: {e}")
