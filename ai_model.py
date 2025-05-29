357c53b54238361d1c4293906cb27fb552601e1a
dogukan0640 <otay0353@gmail.com>
2025-05-30 00:29:55 +0300

    Delete ai_model.py

--- ai_model.py
+++
@@ -1,9 +0,0 @@
-
-import joblib
-import pandas as pd
-
-def load_model():
-    return joblib.load("model.pkl")
-
-def predict(model, df):
-    return model.predict_proba(df[["fr", "oi", "cvd", "price"]])[:, 1]
